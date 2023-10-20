a9e5889fb5d21ac1b317628737ae96346b870843
Habyeok <earnest_hb@naver.com>
2023-10-20 14:12:33 +0900

    html mark up 정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// HTML
+// Mark up
+// <div> <p> hello world </p> </div>
+// 태그로 구현된 것
+// 화면을 구상하기 위한 규약
+
+
+// CSS
+// 꾸미기 언어
+
+// JS
+// HTML을 조작하는 언어
+
 
 function App() {
+  // react way
+  const [todos, setTodos] = useState(["할일 1", "할일 2"]);
+  // state 변화하는 값
+  // React -> state가 변할 때마다 화면을 다시 그린다.
+  // ["할일 1", "할일 2"];
+
   return (
+    // JSX (JS -> HTML)
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST</h1>
+      <div>
+        <input />
+        <button>ADD</button>
+      </div>
+      {/* DRY Don't Repeat Yourself */}
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <input type="checkbox" />
+          <span>{todo}</span>
+          <button>DEL</button>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
